ebe58fa08629fb026a0e52bbc040cd6f8fafc1c2
bertukan <bertukan2017@gmail.com>
2018-07-14 11:58:47 -0500

    initaila commit

---
+++ Backend/Model/user.js
@@ -0,0 +1,17 @@
+
+class User{
+
+    constructor(accountId, name, age, salary, Email){
+
+    var users={};
+
+    this.accountId= accountId;
+    this.name=name;
+    this.age=age;
+    this.salary=salary;
+    this.Email=Email;
+
+}
+}
+
+module.exports=User
